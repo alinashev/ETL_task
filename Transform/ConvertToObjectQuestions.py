c0e5f36dd4eff5c5a9e06e610c6190d42f24f061
alinashev <al.t.shevchenko@gmail.com>
2021-11-11 16:10:09 +0200

    Added Iterator class

--- Transform/ConvertToObjectQuestions.py
+++ Transform/ConvertToObjectQuestions.py
@@ -1,5 +1,6 @@
 from Entities.Questions.questions import Question
 from Entities.Responses.metadata import Metadata
+from Service.iterator import Iterator
 
 
 class ConvertToObjectQuestions:
@@ -8,34 +9,27 @@ class ConvertToObjectQuestions:
     def convert_questions(json_string):
         array_json = json_string['questions']
         list_of_question_obj = list()
-        iterator = iter(array_json)
-        has_next = True
-        while has_next:
-            try:
-                current = next(iterator)
-                list_of_question_obj.append(Question(current['id'], current['question'], current['field_id'], None))
-            except StopIteration:
-                has_next = False
+
+        iterator = Iterator(array_json)
+        while iterator.has_next():
+            current = iterator.next()
+            list_of_question_obj.append(Question(current['id'], current['question'], current['field_id'], None))
         return list_of_question_obj
 
     @staticmethod
     def convert_metadata(json_string):
         array_json = json_string['responses']
         list_of_metadata_obj = list()
-        iterator = iter(array_json)
-        has_next = True
-        while has_next:
-            try:
-                current = next(iterator)['metadata']
-                list_of_metadata_obj.append(Metadata(current['browser'],
-                                                     current['platform'],
-                                                     current['date_land'],
-                                                     current['date_submit'],
-                                                     current['user_agent'],
-                                                     current['referer'],
-                                                     current['network_id']
-                                                     ))
 
-            except StopIteration:
-                has_next = False
+        iterator = Iterator(array_json)
+        while iterator.has_next():
+            current = iterator.next()['metadata']
+            list_of_metadata_obj.append(Metadata(current['browser'],
+                                                 current['platform'],
+                                                 current['date_land'],
+                                                 current['date_submit'],
+                                                 current['user_agent'],
+                                                 current['referer'],
+                                                 current['network_id']
+                                                 ))
         return list_of_metadata_obj
