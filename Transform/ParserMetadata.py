242b4e66af3c5238b45ade3f143ed58ff5bc3e42
alinashev <al.t.shevchenko@gmail.com>
2021-11-15 14:31:44 +0200

    Added list generation

--- Transform/ParserMetadata.py
+++ Transform/ParserMetadata.py
@@ -4,9 +4,4 @@ from Entities.metadata import Metadata
 
 class ParserMetadata(Parser):
     def parse_to_obj(self, json_string):
-        array_json = json_string['responses']
-        list_of_metadata_obj = list()
-        for i in array_json:
-            current = i['metadata']
-            list_of_metadata_obj.append(Metadata(**current))
-        return list_of_metadata_obj
+        return (Metadata(**i['metadata']) for i in json_string['responses'])
--- Transform/ParserQuestions.py
+++ Transform/ParserQuestions.py
@@ -5,8 +5,4 @@ from Entities.questions import Question
 class ParserQuestions(Parser):
 
     def parse_to_obj(self, json_string):
-        array_json = json_string['questions']
-        list_of_question_obj = list()
-        for i in array_json:
-            list_of_question_obj.append(Question(i['id'], i['question'], i['field_id'], None))
-        return list_of_question_obj
+        return (Question(i['id'], i['question'], i['field_id'], None) for i in json_string['questions'])
